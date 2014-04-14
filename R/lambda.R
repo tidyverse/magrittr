8bd28b272ed367e9747018b22745b16fd8f6e4f2
smbache <smbache>
2014-04-14 13:38:37 +0200

    new lambda syntax
    changed lambda syntax

--- R/lambda.R
+++ R/lambda.R
@@ -6,38 +6,52 @@
 #' (i.e. it takes a value as the first argument to be fed into the function),
 #' and it avoids the high precedence of the \code{function} keyword.
 #'
+#' \code{lambda} has a special syntax, where the expression is defined as
+#' \code{symbol -> expression}. The alias \code{l} is shorthand for \code{lambda}.
+#' If both \code{value} and \code{expression} is provided, the resulting function
+#' is evaluated with \code{value} as input; otherwise the function is returned.
+#'
 #' @param value the value to be fed into the function
-#' @param body the body of the anonymous function
-#' @param arg the name used to reference \code{value} in the function. Defaults to x.
+#' @param expr A special kind of expression for the anonymous function.
+#'   The syntax is \code{symbol -> expression}, see the examples.
 #' @return The anonymous function is evaluated and the result is returned
 #'   (unless \code{value} is missing, in which case the function itself is
 #'    returned.)
 #' @rdname lambda
 #' @export
 #' @examples
-#' iris %>%
-#'   lambda(rbind(x %>% head, x %>% tail))
+#' lambda(x -> x^2 + 2*x)
+#'
+#' sapply(1:10, lambda(x -> x^2))
+#'
+#' Filter(lambda(x -> x > 0), rnorm(100))
 #'
 #' iris %>%
-#'   lambda(rbind(z %>% head, z %>% tail), z)
+#'   lambda(dfr -> rbind(dfr %>% head, dfr %>% tail))
 #'
 #' 1:10 %>%
 #'   sin %>%
-#'   lambda({ d <- abs(x) > 0.5; x*d })
-lambda <- function(value, body, arg = "x")
+#'   lambda(x -> { d <- abs(x) > 0.5; x*d })
+lambda <- function(value, expr)
 {
-  # Capture the input arguments
-  body <- substitute(body)
-  arg  <- as.character(substitute(arg))
+
+  shift <- missing(expr)
+  expr  <- if (shift) substitute(value) else substitute(expr)
+
+  if (!identical(expr[[1]], quote(`<-`)))
+    stop("Malformed expression. Expected format is symbol -> expression.")
+  if (!is.symbol(expr[[3]]))
+    stop("Malformed expression. 'sym' in 'sym -> expression' is not a symbol.")
 
   # Construct the argument pairlist
-  pl   <- as.pairlist({ al <- list(); `[[<-`(al, arg, ) })
+  pl   <- as.pairlist({ al <- list(); `[[<-`(al, as.character(expr[[3]]), ) })
 
-  result <- eval(call("function", pl, body), parent.frame(), parent.frame())
-  if (missing(value)) result else result(value)
+  result <- eval(call("function", pl, expr[[2]]), parent.frame(), parent.frame())
+  if (shift) result else result(value)
 }
 
 #' @rdname aliases
 #' @usage NULL
 #' @export
 l <- lambda
+
--- R/tee.R
+++ R/tee.R
@@ -6,13 +6,13 @@
 #' certain steps in a chain.
 #'
 #' @param lhs That which is to be piped
-#' @param rhs a function call used for its side effect. 
+#' @param rhs a function call used for its side effect.
 #' @return lhs
 #' @rdname tee
 #' @export
 #' @examples
-#' 1:10 %T>% 
-#'    lambda(cat("LHS:", x, "\n")) %>% 
+#' 1:10 %T>%
+#'    lambda(x -> cat("LHS:", x, "\n")) %>%
 #'    multiply_by(2)
 `%T>%` <- pipe(tee = TRUE)
 
@@ -21,7 +21,7 @@
 #' This is useful e.g. for logging, plotting, or printing
 #' each step in a pipeline.
 #'
-#' @param fun a function which will be evaluated with 
+#' @param fun a function which will be evaluated with
 #'  left-hand side of a pipe expression as argument.
 #' @return a pipe operator with the attached side effect.
 #' @rdname pipe_with
@@ -33,17 +33,16 @@
 #'
 #' # Create a pipe using the logger.
 #' `%L>%` <- pipe_with(logger)
-#' 
+#'
 #' # Test it:
-#' iris %L>% 
-#'    subset(Species == "setosa") %L>%  
-#'    subset(Sepal.Length > 5) %L>% 
+#' iris %L>%
+#'    subset(Species == "setosa") %L>%
+#'    subset(Sepal.Length > 5) %L>%
 #'    tail(10)
-pipe_with <- 
+pipe_with <-
   function(fun)
     if (!is.function(fun)) {
       stop("Argument `fun` must be a function.")
     } else {
       pipe(tee = fun)
     }
-    
