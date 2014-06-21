c50470995c7d36b09743c3f057ff2a8a8a531869
smbache <stefan@stefanbache.dk>
2014-06-21 08:52:57 +0200

    integrate %$% with pipe operations.

--- R/expose.R
+++ R/expose.R
@@ -8,20 +8,56 @@
 #' @param lhs a data.frame or list.
 #' @param rhs a call/expression.
 #' @usage lhs \%$\% rhs
-#' @rdname wrap
+#' @rdname expose
 #' @export
-`%$%` <- function(lhs, rhs) {
-
-  # Check that the left-hand side expression is a data.frame or list.
-  if (!is.data.frame(lhs) && !is.list(lhs))
-    stop("left-hand side must be a list or data.frame.", call. = FALSE)
-
-  # Check that the right-hand side is a call.
+`%$%` <- function(lhs, rhs)
+{
+  # Capture unevaluated arguments
   rhs <- substitute(rhs)
+
+  # reference the parent frame
+  parent <- parent.frame()
+
+  # Check right-hand side
   if (!is.call(rhs) && !is.symbol(rhs))
-    stop("right-hand side must be symbol or a call.")
+    stop("RHS should be a symbol, a call, or a function.")
 
-  # Evaluate the expression, exposing the contents of the left-hand side.
-  eval(rhs, lhs, parent.frame())
+  # Get an environment for evaluation of left-hand side.
+  if (exists("__env__", parent, mode = "environment", inherits = FALSE)) {
 
+    # get the existing environment and flag this as not being top-level
+    env <- get("__env__", parent)
+
+    # If it is locked, make a new one. Since compound is passed along in this
+    # case, we require that toplevel still be FALSE.
+    if (env[["__locked__"]])
+      env <- pipe_env(parent, compound = env[["__compound__"]])
+    toplevel <- FALSE
+
+  } else {
+
+    # Create a new environment, and set top-level
+    env <- pipe_env(parent)
+    toplevel <- TRUE
+
+  }
+
+  # From now on, this environment cannot be re-used.
+  env[["__locked__"]] <- TRUE
+
+  # Evaluate the call
+  res       <- withVisible(eval(rhs, lhs, parent))
+  visibly   <- res$visible
+  to.return <- res$value
+
+  if (toplevel && !is.null(env[["__compound__"]])) {
+
+    # Compound operator was used, so assign the result, rather than return it.
+    eval(call("<-", get("__compound__", env), to.return), parent, parent)
+
+  } else {
+
+    if (visibly) to.return else invisible(to.return)
+
+  }
 }
