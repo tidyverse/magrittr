2d079feb83ce7650ec7c59ab9da913dd612f938f
Gabor Csardi <csardi.gabor@gmail.com>
2015-07-11 18:02:30 -0400

    Test case: the first stage is not the pipe
    We need to look for the pipe, obviously....

--- tests/testthat/test-tamper.R
+++ tests/testthat/test-tamper.R
@@ -18,3 +18,25 @@ test_that("We can detect if we are in a pipe", {
     divide_by(5)
 
 })
+
+test_that("What if the pipe is in a function", {
+
+  skip(message = "not ready yet for a real test")
+
+  old_handler <- getOption("error")
+  on.exit(options(error = old_handler))
+  options(error = tamper)
+
+  f <- function(data) {
+    data %>%
+      (function(x) force(x)) %>%
+      multiply_by(10) %>%
+      add(10) %>%
+      add("oh no!") %>%
+      subtract(5) %>%
+      divide_by(5)
+  }
+
+  f(1:10)
+
+})
