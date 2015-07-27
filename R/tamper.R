7a140130a9dab5c12d5b5950342392c121d9a7bd
Gabor Csardi <csardi.gabor@gmail.com>
2015-07-27 17:10:26 -0400

    Refactor tamper, to allow dump_pipes()

--- R/tamper.R
+++ R/tamper.R
@@ -59,58 +59,6 @@ tamper <- function() {
     on.exit(tracingState(tState))
   }
 
-  calls <- sys.calls()
-  from <- 0L
-  no_calls <- length(calls)
-
-  freduce_calls <- get_pipe_stages(calls)
-  pipe_call <- get_last_pipe_call(calls)
-  freduce_calls <- freduce_calls[ freduce_calls > pipe_call ]
-  if (is.na(pipe_call)) {
-    ## tracing is handled in recover(), so put it back
-    tracingState(tState)
-    return(recover())
-  }
-
-  ## find an interesting environment to start from
-  ## options(error = tamper) produces a call to this function as an object
-  calls <- calls[- length(calls)]
-  no_calls <- no_calls - 1L
-
-  ## look for a call inserted by trace() (and don't show frames below)
-  ## this level.
-  for (i in rev(seq_len(no_calls))) {
-    calli <- calls[[i]]
-    fname <- calli[[1L]]
-    ## deparse can use more than one line
-    if (!is.na(match(deparse(fname)[1L],
-                     c("methods::.doTrace", ".doTrace")))) {
-      from <- i - 1L
-      break
-    }
-  }
-
-  ## if no trace, look for the first frame from the bottom that is not
-  ## stop, recover or tamper
-  if (from == 0L) {
-    for (i in rev(seq_len(no_calls))) {
-      calli <- calls[[i]]
-      fname <- calli[[1L]]
-      if (!is.name(fname) ||
-          is.na(match(as.character(fname),
-                      c("tamper", "recover", "stop", "Stop")))) {
-        from <- i
-        break
-      }
-    }
-  }
-
-  ## We return here if there are no frames.
-  if (from <= 0L) {
-    cat(gettext("No suitable frames for tamper()\n"))
-    return()
-  }
-
   if (! interactive()) {
     try({
       dump.frames()
@@ -123,19 +71,25 @@ tamper <- function() {
     return(NULL)
   }
 
-  ## ---------------------------------------------------------------
-  ## This is the main stuff
+  dump <- get_pipe_calls(sys.calls(), sys.frames())
 
-  ## Get the pieces of the pipe, convert to character for printing
-  chr_chain_parts <- chain_parts_to_chr(
-    get("chain_parts", envir = sys.frame(pipe_call))
-  )
+  if (is.na(dump$pipe_call)) {
+    ## tracing is handled in recover(), so put it back
+    tracingState(tState)
+    return(recover())
+  }
 
-  ## Get the bad pipe stage
-  bad_stage <- get_bad_stage(freduce_calls)
+  ## We return here if there are no frames.
+  if (dump$from <= 0L) {
+    cat(gettext("No suitable frames for tamper()\n"))
+    return()
+  }
 
-  ## Nice printout of pipe stages
-  chr_stages <- format_pipe_stages(chr_chain_parts, bad_stage)
+  pipe_menu(dump)
+
+}
+
+pipe_menu <- function(dump) {
 
   ## Interactive menu
   pipe_stack <- TRUE
@@ -143,7 +97,7 @@ tamper <- function() {
               "\nEnter a pipe stage number, 1 to switch mode, or 0 to exit  ")
   other_text <- c("Show pipe stages\n", "Show full stack frames\n")
 
-  stacks <- list(calls, chr_stages)
+  stacks <- list(dump$calls, dump$chr_stages)
 
   repeat {
 
@@ -156,14 +110,14 @@ tamper <- function() {
 
     } else if (pipe_stack && which == 2) {
       eval(substitute(browser(skipCalls = skip),
-                      list(skip = 7 - pipe_call)),
-           envir = sys.frame(pipe_call))
+                      list(skip = 7 - dump$pipe_call)),
+           envir = sys.frame(dump$pipe_call))
 
     } else if (which > 1 && pipe_stack) {
       which <- which - 2
       eval(substitute(browser(skipCalls = skip),
-                      list(skip = 7 - freduce_calls[which])),
-           envir = sys.frame(freduce_calls[which]))
+                      list(skip = 7 - dump$freduce_calls[which])),
+           envir = sys.frame(dump$freduce_calls[which]))
 
     } else if (which > 1 && ! pipe_stack) {
       which <- which - 1
@@ -177,73 +131,4 @@ tamper <- function() {
 
   ## ---------------------------------------------------------------
 
-}
-
-is_freduce_call <- function(x) identical(x[[1L]], quote(freduce))
-
-get_pipe_stages <- function(calls) {
-  which(vapply(calls, is_freduce_call, logical(1)))
-}
-
-is_pipe_call <- function(x) is_pipe(x[[1L]])
-
-get_last_pipe_call <- function(calls) {
-  res <- tail(which(vapply(calls, is_pipe_call, logical(1))), 1)
-  if (length(res)) res else NA_integer_
-}
-
-chain_parts_to_chr <- function(chain_parts) {
-  no_pipes <- length(chain_parts$pipes)
-  list(
-    lhs = paste(deparse(chain_parts$lhs), collapse = "\n    "),
-    pipes = vapply(chain_parts$pipes, as.character, ""),
-    rhss = vapply(lapply(chain_parts$rhss, deparse),
-      paste, "", collapse = "\n         ")
-  )
-}
-
-get_bad_stage <- function(freduce_calls) {
-
-  ## ---------------------------------------------------------------
-  ## We might be more down from the actual error in the pipe chain,
-  ## because of promises. For example if `value` is a promise all
-  ## along the chain, then we are at the last stage.
-  ##
-  ## We have no_pipes stages in total, and we have
-  ## length(freduce_calls) stages in the stack, these
-  ## correspond to the first length(freduce_calls) stages.
-  ##
-  ##        |--------|--------|--------|--------|--------|
-  ## pipes: |   1    |   2    |   3    |   4    |    5   |
-  ## stack: |   1    |   2    |   3    |
-  ##
-  ## Now we find which stage the error corresponds to. We need
-  ## to go up in the stack, until we find `value` evaluated.
-
-  ## Checks which stacks are below the error in reality.
-  ## If the `value` promise is not evaled, then we are below.
-  is_below_error <- substitute(
-    inherits(try(value, silent = TRUE), "try-error")
-  )
-
-  bad_stage <- length(freduce_calls)
-  while (bad_stage > 0 &&
-           eval(is_below_error,
-                envir = sys.frame(freduce_calls[bad_stage]))) {
-    bad_stage <- bad_stage - 1
-  }
-
-  bad_stage
-}
-
-format_pipe_stages <- function(chr_chain_parts, bad_stage) {
-  no_pipes <- length(chr_chain_parts$rhss)
-  markers <- rep("  ", no_pipes + 1)
-  markers[bad_stage + 1] <- "->"
-
-  paste(
-    markers,
-    c(chr_chain_parts$lhs, paste0("  ", chr_chain_parts$rhss)),
-    c(chr_chain_parts$pipes, "")
-  )
 }
