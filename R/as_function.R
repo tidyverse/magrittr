44ef23c5dd923ec61fbad226cb34751819fac42d
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-09 12:20:36 +0200

    take care of visibility

--- R/as_function.R
+++ R/as_function.R
@@ -1,8 +1,8 @@
 #' @export
 as.function.fseq <- function(fseq) {
   bodies <- lapply(functions(fseq), body)
-  assignments <- lapply(bodies, function(b) call("<-", quote(.), b))
-  chain_body <- as.call(c(quote(`{`), assignments, quote(.)))
+  assignments <- lapply(bodies[-length(bodies)], function(b) call("<-", quote(.), b))
+  chain_body <- as.call(c(quote(`{`), assignments, bodies[length(bodies)]))
   nice_pipe <- as.function(c(alist(.=), chain_body))
   environment(nice_pipe) <- parent.env(environment(fseq))
   nice_pipe
