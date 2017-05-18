82b1c927b463211a3e0abf2389c729d372374d2c
hadley <h.wickham@gmail.com>
2017-05-18 17:00:36 -0500

    Update package docs

--- R/magrittr.R
+++ R/magrittr.R
@@ -12,13 +12,13 @@
 #' The operators pipe their left-hand side values forward into expressions that
 #' appear on the right-hand side, i.e. one can replace \code{f(x)} with 
 #' \code{x \%>\% f}, where \code{\%>\%} is the (main) pipe-operator.
-#' \cr\cr
+#' 
 #' Consider the example below. Four operations are performed to 
 #' arrive at the desired data set, and they are written in a natural order: 
 #' the same as the order of execution. Also, no temporary variables are needed.
 #' If yet another operation is required, it is straight-forward to add to the
 #' sequence of operations whereever it may be needed.
-#' \cr\cr
+#' 
 #' For a more detailed introduction see the vignette 
 #' (\code{vignette("magrittr")}) or the documentation pages for the
 #' available operators:\cr
@@ -38,6 +38,5 @@
 #'   transform(variable_c = variable_a/veraiable_b) %>%
 #'   head(100)
 #' }
-#' @docType package
-#' @name magrittr
-NULL
+#' @keywords internal
+"_PACKAGE"
