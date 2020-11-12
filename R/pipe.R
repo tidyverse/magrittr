bd0ea34d8a01163c142f5ffaa8dcadc29970a08b
Lionel Henry <lionel.hry@gmail.com>
2020-11-12 09:09:49 +0100

    Add gender FAQ page about the gender of the pipe (#230)
    Closes #221

--- R/pipe.R
+++ R/pipe.R
@@ -303,3 +303,16 @@ lambda_fmls <- as.pairlist(alist(. = ))
 as_pipe_fn <- function(expr, env) {
   eval(call("function", lambda_fmls, expr), env)
 }
+
+#' FAQ: What is the gender of the pipe?
+#'
+#' In Magritte's original quote "Ceci n'est pas une pipe," the word
+#' "pipe" is feminine. However the magrittr package quotes it as
+#' "Ceci n'est pas un pipe," with a masculine "pipe." This
+#' lighthearted misappropriation is intentional. Whereas the
+#' object represented in Magritte's painting (a pipe that you can
+#' smoke) is feminine in the French language, a computer pipe
+#' (which is an Anglicism in French) is masculine.
+#'
+#' @name faq-pipe-gender
+NULL
